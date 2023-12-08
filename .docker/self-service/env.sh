e98caef6dd8860bf39c7667200953b483d57fa66
tan <thanos.geo@proton.me>
2023-12-08 21:02:57 +0200

    the Self-Service UI requires both KRATOS_PUBLIC_URL & KRATOS_BROWSER_URL Now plays well/fixed.

--- .docker/self-service/env.sh
+++ .docker/self-service/env.sh
@@ -1,7 +1,9 @@
 export PORT=4455
-export SECURITY_MODE=
+export SECURITY_MODE=standalone
 export KRATOS_BROWSER_URL=${AUTHN_PUBLIC_URL}
-
+export KRATOS_PUBLIC_URL=${AUTHN_PUBLIC_URL}
+export KRATOS_ADMIN_URL=${AUTHN_ADMIN_URL}
+export HYDRA_ADMIN_URL=${AUTHZ_ADMIN_URL}
 
 export COOKIE_SECRET=d3223dm23o-sdf=-sdvfsd=-a9=-934
 export CSRF_COOKIE_NAME=casablanca_csrf_cookie
